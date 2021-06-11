feabf160dc69f362458736ecd7420923c250197c
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-06-10 17:50:18 -0700

    Let's talk design card complete

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  $("#me-right").click(function() {
+    if($("#me-right").hasClass("back")){
+      $('#me-right').toggleClass('front');
+      $('#me-right').toggleClass('back');
+      $('#me-left').toggleClass('front');
+      $('#me-left').toggleClass('back');
+      //Right
+      $('.mrBack-B').toggleClass('mrBack-F');
+      $('.mrSqr-B').toggleClass('mrSqr-F');
+      $('.mrCrc-B').toggleClass('mrCrc-F');
+      $('.mrBar-B').toggleClass('mrBar-F');
+      $('.mr-CrcBr-B').toggleClass('mr-CrcBr-F');
+      $('.mrTrg-B').toggleClass('mrTrg-F');
+      $('.mriTrg-B').toggleClass('mriTrg-F');
+      $('.mrt1-B').toggleClass('mrt1-F');
+      $('.mrt2-B').toggleClass('mrt2-F');
+
+      $('.mr-shadow-B').toggleClass('mr-shadow-F');
+      $('.mright-para-B').toggleClass('mright-para-F');
+
+      //Left
+      $('.mlBack-B').toggleClass('mlBack-F');
+      $('.mlt1-B').toggleClass('mlt1-F');
+      $('.mlt2-B').toggleClass('mlt2-F');
+      $('.mleft-para-B').toggleClass('mleft-para-F');
+    }
+    
+  });
+
+  $("#me-left").click(function() {
+    if($("#me-left").hasClass("back")){
+      $('#me-right').toggleClass('front');
+      $('#me-right').toggleClass('back');
+      $('#me-left').toggleClass('front');
+      $('#me-left').toggleClass('back');
+      //Right
+      $('.mrBack-B').toggleClass('mrBack-F');
+      $('.mrSqr-B').toggleClass('mrSqr-F');
+      $('.mrCrc-B').toggleClass('mrCrc-F');
+      $('.mrBar-B').toggleClass('mrBar-F');
+      $('.mr-CrcBr-B').toggleClass('mr-CrcBr-F');
+      $('.mrTrg-B').toggleClass('mrTrg-F');
+      $('.mriTrg-B').toggleClass('mriTrg-F');
+      $('.mrt1-B').toggleClass('mrt1-F');
+      $('.mrt2-B').toggleClass('mrt2-F');
+      $('.mr-shadow-B').toggleClass('mr-shadow-F');
+      $('.mright-para-B').toggleClass('mright-para-F');
+
+      //Left
+      $('.mlBack-B').toggleClass('mlBack-F');
+      $('.mlt1-B').toggleClass('mlt1-F');
+      $('.mlt2-B').toggleClass('mlt2-F');
+      $('.mleft-para-B').toggleClass('mleft-para-F');
+    }
+  });
+
+  
+
+
+});
