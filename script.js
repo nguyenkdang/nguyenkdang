17d7dc5570aff8f18ccfaf9d60dd53467c63eee2
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-06-23 03:42:35 -0700

    updated footer

--- script.js
+++ script.js
@@ -75,3 +75,12 @@ $(document).ready(function() {
 
 
 });
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-transition-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-transition-stopper");
+  }, 400);
+});
