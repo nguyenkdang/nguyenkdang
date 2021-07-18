28b96fdc1fda4bd26766ac3d0b9098e69211391f
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-07-18 01:09:38 -0700

    Third party endpoint integration

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,26 +5,25 @@ const Contact = () => {
     return (
         <div className="contact">
             <Socials className="LA"/>
-            <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+            <form id="fcf-form-id" class="fcf-form-class" method="post" action="https://formsubmit.co/nguyenkhdang@gmail.com">
              <div id="contact_box">
+                <input type="hidden" name="_captcha" value="false"/>
+                <input type="hidden" name="_next" value="https://www.nguyenkdang.com/contact"/>
                 <div id="name_field">
                     <h2 class="inp_label">name </h2>
-                    <label for="Name" class="fcf-label">Your name</label>
                     <input type="text" id="Name" name="Name" class="fcf-form-control" required/>
                 </div>
                 <div id="email_field">
                     <h2 class="inp_label">email </h2>
-                    <label for="Email" class="fcf-label">Your email address</label>
                     <input type="email" id="Email" name="Email" class="fcf-form-control" required/>
                 </div>
                 
                 <div id="message_field">
-                    <label for="Message" class="fcf-label">Your message</label>
                     <h2 class="inp_label">message</h2>
                     <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
                 </div>
                 
-                <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
+                <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send</button>
             </div>
             </form>
         </div>
