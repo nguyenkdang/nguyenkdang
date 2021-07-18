ac4ee55bc2994c85fb190787e87bf723f9b497d9
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-07-18 00:29:57 -0700

    attempt php update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,21 +8,23 @@ const Contact = () => {
             <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
              <div id="contact_box">
                 <div id="name_field">
-                    
                     <h2 class="inp_label">name </h2>
-                    <input type="text" id="Name_inp" name="Name" class="fcf-form-control" required/> 
+                    <label for="Name" class="fcf-label">Your name</label>
+                    <input type="text" id="Name" name="Name" class="fcf-form-control" required/>
                 </div>
                 <div id="email_field">
                     <h2 class="inp_label">email </h2>
-                    <input type="email" id="Email_inp" name="Email" class="fcf-form-control" required/>
+                    <label for="Email" class="fcf-label">Your email address</label>
+                    <input type="email" id="Email" name="Email" class="fcf-form-control" required/>
                 </div>
                 
                 <div id="message_field">
+                    <label for="Message" class="fcf-label">Your message</label>
                     <h2 class="inp_label">message</h2>
                     <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
                 </div>
                 
-            <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">send</button>
+                <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
             </div>
             </form>
         </div>
