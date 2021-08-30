67051d082350794df222e789b018d5c6e9513033
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-08-30 03:56:26 -0700

    revert home

--- src/components/home/projects/projectBadge.js
+++ src/components/home/projects/projectBadge.js
@@ -1,8 +1,30 @@
+import './ProjectBadge.scss'
+import { Link } from 'react-router-dom';
 
 const ProjectBadge = ({title, langLogo, desc,  pos, setprojScroll}) => {
     return (
         <div className='ProjectBadge'>
-            
+            <div className='badgeBack'>
+                <Link onClick={()=> setprojScroll(pos)} to="projects"> 
+                    <div className="infoSec">
+                        <div className="infoSec-back"></div>
+                        <div className="infoCircle">
+                            <h3 className="info-symbol">i </h3>  
+                        </div>
+                        <h2 className='moreinfo'> more info</h2>
+                    </div>
+                </Link>
+                
+                <div className="insideLine">
+                    <p className="bdage-title"> {title} </p>
+                    <p className="bdage-desc"> {desc} </p>
+                </div>
+                
+            </div>
+            <div className="badgeArmor"></div>
+            <div className="badgeBeak">
+                <div className="langLogo"> {langLogo} </div>
+            </div>
         </div>
     )
 }
