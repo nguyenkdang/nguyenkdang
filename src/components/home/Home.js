37feaba928fda888db453355b648993bea538a75
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-08-30 03:40:58 -0700

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,7 @@
 import './Home.scss';
-import About_code from './about/About_code';
-import About_dsgn from './about/About_dsgn';
-import Paint from './intro/Paint';
-import ProjectBadge from './projects/ProjectBadge';
-import PythonLogo from './projects/language/PythonLogo';
+import About_code from './about/About_code'
+import About_dsgn from './about/About_dsgn'
+import Paint from './intro/Paint'
 import Mylogo from '../../svg/Mylogo'
 import { useState} from "react";
 
