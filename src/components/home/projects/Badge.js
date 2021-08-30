bb20a1cbf7834342aff6582d6a043c4ccaee2cbd
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-08-30 04:05:37 -0700

    Update Badge.js

--- src/components/home/projects/Badge.js
+++ src/components/home/projects/Badge.js
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom';
 
-const Badge = () => {
+const Badge = ({title, langLogo, desc,  pos, setprojScroll}) => {
     return (
         <div>
-            
+            <div className='badgeBack'>
+                <Link onClick={()=> setprojScroll(pos)} to="projects"> 
+                    <div className="infoSec">
+                        <div className="infoSec-back"></div>
+                        <div className="infoCircle">
+                            <h3 className="info-symbol">i </h3>  
+                        </div>
+                        <h2 className='moreinfo'> more info</h2>
+                    </div>
+                </Link>
+                
+                <div className="insideLine">
+                    <p className="bdage-title"> {title} </p>
+                    <p className="bdage-desc"> {desc} </p>
+                </div>
+                
+            </div>
+            <div className="badgeArmor"></div>
+            <div className="badgeBeak">
+                <div className="langLogo"> {langLogo} </div>
+            </div>
         </div>
     )
 }
