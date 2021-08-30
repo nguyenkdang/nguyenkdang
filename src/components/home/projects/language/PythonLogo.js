e957245015033dbb123964ee9602fba26fbadf4b
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-08-30 03:28:56 -0700

    Updated Home

---
+++ src/components/home/projects/language/PythonLogo.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const PythonLogo = ({size}) => {
+    return (
+        <div>
+            <svg version="1.1" className="python-logo" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+                    x="0px" y="0px" width={size} height={size} viewBox="0 0 110.4 109.8">
+            <style type="text/css">
+                {'.st0{fill:#FFFFFF;}'}
+            </style>
+            <g className="layer1" transform="translate(-473.36088,-251.72485)">
+                <g className="g1894" transform="translate(428.42338,184.2561)">
+                    <path className="path8615 st0" d="M99.8,67.5c-28,0-26.3,12.2-26.3,12.2l0,12.6h26.8V96H62.9c0,0-17.9-2-17.9,26.2
+                        c0,28.3,15.7,27.3,15.7,27.3h9.3v-13.1c0,0-0.5-15.7,15.4-15.7s26.5,0,26.5,0s14.9,0.2,14.9-14.4s0-24.2,0-24.2
+                        S129,67.5,99.8,67.5z M85,75.9c2.7,0,4.8,2.2,4.8,4.8s-2.2,4.8-4.8,4.8s-4.8-2.2-4.8-4.8S82.3,75.9,85,75.9z"/>
+                    <path className="path8620 st0" d="M100.5,177.3c28,0,26.3-12.2,26.3-12.2l0-12.6H100v-3.8h37.4c0,0,17.9,2,17.9-26.3
+                        c0-28.3-15.7-27.3-15.7-27.3h-9.3v13.1c0,0,0.5,15.7-15.4,15.7s-26.5,0-26.5,0s-14.9-0.2-14.9,14.4c0,14.6,0,24.2,0,24.2
+                        S71.3,177.3,100.5,177.3z M115.3,168.8c-2.7,0-4.8-2.2-4.8-4.8s2.2-4.8,4.8-4.8s4.8,2.2,4.8,4.8
+                        C120.1,166.7,118,168.8,115.3,168.8z"/>
+                </g>
+            </g>
+            </svg>
+        </div>
+    )
+}
+
+export default PythonLogo
