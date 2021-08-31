2dfbf06b253e411bffc519459346e0c3f041d484
nguyenkdang <57109202+nguyenkdang@users.noreply.github.com>
2021-08-30 21:28:35 -0700

    resume update

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -5,17 +5,17 @@ import rPDF from './resume.pdf'
 const Resume = () => {
     return (
         <div className="resume">
-          <div id="page" className="indent">
+          <div className="indent page">
             <div className="dl"> 
               <a href={rPDF} download="Nguyen Dang - Resume.pdf">
                 <DlButton size="3.5vw"/>
               </a>
             </div>
             <div>
-              <h1 id="name">Nguyen K. Dang</h1>
-              <h3 id="school">University of California Irvine Graduate</h3>
+              <h1 className="name">Nguyen K. Dang</h1>
+              <h3 className="school">University of California Irvine Graduate</h3>
             </div>
-            <div id="contact">
+            <div className="contact">
               <div className="same_ln">
                 <h3 className="contact_info"> <b>Phone</b> 408-513-7511</h3>
                 <h3 className="contact_info"> <b>Email</b> nguyenkhdang@gmail.com</h3>
@@ -25,17 +25,17 @@ const Resume = () => {
                 <h3 className="contact_info"> <b>Linkedin</b> <a className="link" href="https://www.linkedin.com/in/nguyenkdang/" target="_blank" rel="noreferrer"> linkedin.com/in/nguyenkdang</a> </h3>
               </div>
             </div>
-            <span id="education_break"></span>
+            <span className="education_break"></span>
   
-            <div id="education">
+            <div className="education">
               <h2>Education</h2>
               <div className="line"></div>
                 <h3 className="same_ln">2016 - 2020</h3>
                 <h3 className="same_ln">University of California Irvine, information & Computer Science</h3>
             </div>
 
-            <span id="project_break"></span>
-            <div id="projects">
+            <span className="project_break"></span>
+            <div className="projects">
               <h2>Projects</h2>
               <div className="line"></div>
 
@@ -79,10 +79,10 @@ const Resume = () => {
               </div>
             </div>
   
-            <div id="rel-skill">
+            <div className="rel-skill">
               <h2>Relevant Skills</h2>
               <div className="line"></div>
-              <div id="skills"> 
+              <div className="skills"> 
                 <h3 className="info_row"> Python HTML Java CSS C++ JavaScript</h3>
                 <h3 className="info_row">Excel SQL MySQL Figma <span className="sw">Yahoo Finance API</span></h3>
                 <h3 className="info_row"> 
@@ -92,10 +92,10 @@ const Resume = () => {
                 </h3>
               </div>
             </div>
-            <div id="rel-course">
+            <div className="rel-course">
               <h2>Relevant Courses</h2>
               <div className="line"></div>
-              <div id="courses">
+              <div className="courses">
                 <h3 className="info_row"> 
                   <span className="sw">Artificial Intelligence</span> 
                   <span className="sw">Data Management</span>
